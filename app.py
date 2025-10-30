4d3b98ba8341ceac35c62adf20155d4d27f0605f
Karan Singh <godthatback21@gmail.com>
2025-10-30 15:42:45 +0530

    configuration file added for kubernetes

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask , render_template , request
+
+app = Flask(__name__)
+@app.route("/" , methods = ["GET" , "POST"])
+def home():
+    message =""
+    if request.method == "POST":
+        name = request.form.get("name")
+        message = f"Hello {name}, Welcome to Kubernetes test application!!!"
+    return render_template("index.html" , message=message)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0" , port=5000)
